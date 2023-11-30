3150f3e33f26f55b63d40fca192396a75743cdf6
Yusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2023-12-01 08:31:42 +0900

    Initial Commit

---
+++ NestedListView/NestedListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NestedListViewApp.swift
+//  NestedListView
+//
+//  Created by Yusuke Hasegawa on 2023/12/01.
+//
+
+import SwiftUI
+
+@main
+struct NestedListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
